bee5ac93507203724d48670dd97221e62e78b0c5
Elana Olson <elanaolson@gmail.com>
2018-07-31 10:57:36 -0700

    correct and clarify method comments

--- analysisTool.ts
+++ analysisTool.ts
@@ -1,7 +1,8 @@
 /**
- * ngmigration AnalysisTool scans an AngularJS app for certain critera to recommend
- * a particular migration path to Angular. It employs traversing files,
- * regular expressions, a decision tree algorithm, and final recommendations. 
+ * ngMigration Assistant scans an AngularJS application and recommends
+ * a particular migration path to Angular. It looks for good practices
+ * and anti-patterns to help determine which stage in migration the 
+ * application is and which migration path to recommend.
  */
 
 /**
@@ -13,12 +14,10 @@
  */
 
 import * as fs from 'fs';
-import { default as nodesloc } from 'node-sloc';
 import { resolve } from 'dns';
+import { default as nodesloc } from 'node-sloc';
 const gitignore = require('parse-gitignore');
 const glob = require('glob');
-const minimatch = require('minimatch');
-const exec = require('child_process').exec;
 
 export class AnalysisTool {
 
@@ -26,7 +25,7 @@ export class AnalysisTool {
     private VALUE_NOT_FOUND: number = -1;
 
     analysisDetails = {
-        maxCodeLimit: 880, // 880 lines considered 1 month's work of coding 
+        rewriteThreshold: 880, // 880 lines is considered 1 month's work of coding 
         angularElement: false,
         rootScope: false,
         compile: false,
@@ -41,66 +40,76 @@ export class AnalysisTool {
         controllersCount: 0,
         componentDirectivesCount: 0,
         linesOfCode: 0,
-        ignorePaths: [""],
-        ignoreGlobs: [""],
         mapOfFilesToConvert: new Map()
     };
 
-    constructor(path: string) {
+    /**
+     * Calls countLinesOfCode and waits for it to finish executing before calling 
+     * the contained methods: runAnalysis(), runAntiPatternReport() which must 
+     * finish executing before calling runRecommendation().
+     * @param rootpath user input original directory path
+     */
+    constructor(rootpath: string) {
         this.analysisDetails.mapOfFilesToConvert = new Map<String, Array<String>>();
         setTimeout(() => { }, 1000);
-        this.getGlobsFromGitignore(path);
-        this.countLinesOfCode(path, this.buildPathIgnoringGlobs(path))
+        this.countLinesOfCode(rootpath, this.buildPathIgnoringGlobs(rootpath))
             .then(sourceLines => {
                 this.analysisDetails.linesOfCode = sourceLines;
-                this.runAnalysis(path);
+                this.runAnalysis(rootpath);
                 return this.runAntiPatternReport();
             })
             .catch(err => {
-                console.error("Error 2", err);
+                console.error("Error 1: ", err);
             })
             .then(report => {
                 console.log(this.runRecommendation(report));
-                console.log("\x1b[32m%s\x1b[0m", "MaxCodeLimit: " + Math.round(this.analysisDetails.maxCodeLimit) + ", SLOC: " + this.analysisDetails.linesOfCode + "\n");
+                console.log("\x1b[32m%s\x1b[0m", "rewriteThreshold: " + Math.round(this.analysisDetails.rewriteThreshold) + ", SLOC: " + this.analysisDetails.linesOfCode + "\n");
             })
             .catch(err => {
-                console.error("Error 3", err);
+                console.error("Error 2: ", err);
             });
     }
 
     /**
-    * Finds and parses the .gitignore file in your app into an array of ignoreGlobs using parse-gitignore.
-    * Scans filesystem for any matching globs from ignoreGlobs using glob.
-    * Returns all paths to ignore.
-    * @param path 
+     * Builds a new filesystem by removing files matching the ignore globs using glob.
+     * Returns as an array of files and folders.
+     * @param rootPath original directory path
+     */
+    buildPathIgnoringGlobs(rootpath: string) {
+        let ignoreGlobs = this.getGlobsFromGitignore(rootpath);
+        let filesWithoutIgnores = glob.sync("**", { ignore: ignoreGlobs, cwd: rootpath });
+        return filesWithoutIgnores;
+    }
+
+    /**
+    * Parses .gitignore file into an array of ignoreGlobs and appends default ignoreGlobs 
+    * to the array. Filters out patterns starting with ! from the ignoreGlobs array
+    * because ! means to never ignore. Returns the globs to ignore.
+    * @param rootpath original directory path
     */
-    getGlobsFromGitignore(path: string) {
+    getGlobsFromGitignore(rootpath: string): string[] {
+        let allIgnoreGlobs: string[] = [];
         let defaultIgnoreGlobs = [
             'node_modules', 'node_modules/**', '**/node_modules', '**/node_modules/**',
             '.git', '.git/**', '**/.git', '**/.git/**',
             'tsconfig.json', 'tsconfig.json/**', '**/tsconfig.json', '**/tsconfig.json/**',
             'e2e', 'e2e/**', '**/e2e', '**/e2e/**'
         ];
-        this.analysisDetails.ignoreGlobs = [...gitignore(path + "/.gitignore"), ...defaultIgnoreGlobs];
-        this.analysisDetails.ignoreGlobs = this.analysisDetails.ignoreGlobs.filter((pattern) => {
+        allIgnoreGlobs = [...gitignore(rootpath + "/.gitignore"), ...defaultIgnoreGlobs].filter((pattern) => {
             return !pattern.startsWith("!");
         });
+        return allIgnoreGlobs;
     }
 
-    /**
-     * Builds a new filesystem without the ignored files and
-     * returns an array of filesnames.
-     */
-    buildPathIgnoringGlobs(path: string) {
-        let filesWithoutIgnores = glob.sync("**", { ignore: this.analysisDetails.ignoreGlobs, cwd: path });
-        return filesWithoutIgnores;
-    }
-
-    // //DO NOT DELETE
-    // //This scans only git repos, asks git for a list of files to check from the .gitignore file within the given directory.
-    // //Uses grep to ignore the defaultIgnoreGlobs. 
+    // /**
+    //  * DO NOT DELETE - possible for future implementation!
+    //  * Another way of parsing the .gitignore and building a new filesystem. Scans only 
+    //  * git repos and asks git for a list of files to check from the .gitignore file 
+    //  * within the given directory. Uses grep to ignore default ignore globs.
+    //  */
+    // const exec = require('child_process').exec;
     // buildPathIgnoringGlobs(path: string) {
-    //     exec("cd \"" + path + "\" && git ls-tree -r master --name-only | egrep -v \".*(node_modules|e2e|.git|package.json|tsconfig.json).*\"", function (error: string, data: string) {
+    //     exec("cd \"" + path + "\" && git ls-tree -r master --name-only | egrep -v \".*(node_modules|e2e|.git|tsconfig.json).*\"", function (error: string, data: string) {
     //         let a = data.split("\n");
     //         a.pop();
     //         console.log(a);
@@ -108,16 +117,18 @@ export class AnalysisTool {
     // }
 
     /**
-     * Depends on node-sloc to count source lines of code (sloc).
-     * Only scans files with extensions: js, ts, or html. Does not scan node_modules directory.
-     * Returns a promise that resolves to sloc.
+     * Counts the source lines of code (sloc) using node-sloc to traverse through 
+     * the passed in filesystem. Uses filtered filesystem as not count sloc
+     * in node_modules and other ignored files. Returns a promise that resolves to sloc.
+     * @param rootPath original directory path
+     * @param filteredFilePaths filtered filesystem buildPathIgnoringGlobs() returns
     */
-    async countLinesOfCode(rootPath: string, filePaths: string[]): Promise<any> {
+    async countLinesOfCode(rootPath: string, filteredFilePaths: string[]): Promise<any> {
         return new Promise((resolve, reject) => {
             const promisesINeedResolvedForMeToBeDone = [];
 
             let lines: number = 0;
-            for (let file of filePaths) {
+            for (let file of filteredFilePaths) {
                 const options = {
                     path: rootPath + "/" + file,
                     extensions: ['js', 'ts', 'html'],
@@ -140,24 +151,28 @@ export class AnalysisTool {
         });
     }
 
-
-
     /**
-     * Recursively traverses file system and scans each .js/.ts/.html file by calling analysis tests.
-     * Only scan these extensions to avoid scanning node_modules, .json, yarn lock, .md.
-     * Attaches current file to next file to produce correct directory and traverse down the tree.
+     * Traverses through filtered filesystem returned by buildPathIgnoringGlobs()
+     * and calls testFile() to run the individual tests.
+     * @param rootPath original directory path
      */
-    private runAnalysis(path: string) {
-        const list = fs.readdirSync(path);
+    private runAnalysis(rootpath: string) {
+        const list = fs.readdirSync(rootpath);
         let currentPath: string = "";
 
-        for (let fileOrFolder of this.buildPathIgnoringGlobs(path)) {
-            currentPath = path + "/" + fileOrFolder;
+        for (let fileOrFolder of this.buildPathIgnoringGlobs(rootpath)) {
+            console.log("F: " + fileOrFolder);
+            currentPath = rootpath + "/" + fileOrFolder;
+            console.log("C: " + currentPath);
             this.testFile(currentPath);
-            currentPath = fileOrFolder;
         }
     }
 
+    /**
+     * Calls tests on each file in the filtered filesystem to looks for anti-patterns,
+     * Angular version, and additional practices. 
+     * @param currentPath 
+     */
     testFile(currentPath: string) {
         let tests = [
             (filename: string, data: string) => this.checkFileForRootScope(filename, data),
@@ -256,8 +271,9 @@ export class AnalysisTool {
     }
 
     /**
-    * Calculates the maxCodeLimit and generates a preparation report. 
-    * Reports on rootScope, compile, unit tests, scripting language, and component architecture.
+    * Creates the anti-pattern report and calculates the rewriteThreshold runRecommendation() uses.
+    * Each time an anti-pattern is found, general instructions and files needing corrections 
+    * are appended to the preparation report. Returns a promise that resolves to the preparation report. 
     */
     private runAntiPatternReport(): Promise<any> {
         let preparationReport = "The below changes are necessary for preparing your app for upgrading."
@@ -267,23 +283,23 @@ export class AnalysisTool {
 
         if (this.analysisDetails.rootScope) {
             preparationReport += "\n   \x1b[34m*\x1b[0m App contains $rootScope, must refactor rootScope into services.";
-            this.analysisDetails.maxCodeLimit *= this.CODE_LIMIT_MULTIPLIER;
+            this.analysisDetails.rewriteThreshold *= this.CODE_LIMIT_MULTIPLIER;
         }
         if (this.analysisDetails.compile) {
             preparationReport += "\n   \x1b[34m*\x1b[0m App contains $compile, must rewrite compile to eliminate dynamic feature of templates.";
-            this.analysisDetails.maxCodeLimit *= this.CODE_LIMIT_MULTIPLIER;
+            this.analysisDetails.rewriteThreshold *= this.CODE_LIMIT_MULTIPLIER;
         }
         if (!this.analysisDetails.hasUnitTest) {
             preparationReport += "\n   \x1b[34m*\x1b[0m App does not contain unit tests, must write unit tests.";
-            this.analysisDetails.maxCodeLimit *= this.CODE_LIMIT_MULTIPLIER;
+            this.analysisDetails.rewriteThreshold *= this.CODE_LIMIT_MULTIPLIER;
         }
         if (this.analysisDetails.jsFileCount > 0) {
             preparationReport += "\n   \x1b[34m*\x1b[0m App contains " + this.analysisDetails.jsFileCount + " JavaScript files that need to be converted to TypeScript.";
-            this.analysisDetails.maxCodeLimit *= this.CODE_LIMIT_MULTIPLIER;
+            this.analysisDetails.rewriteThreshold *= this.CODE_LIMIT_MULTIPLIER;
         }
         if (this.analysisDetails.controllersCount > 0) {
             preparationReport += "\n   \x1b[34m*\x1b[0m App contains " + this.analysisDetails.controllersCount + " controllers that need to be converted to component directives.";
-            this.analysisDetails.maxCodeLimit *= this.CODE_LIMIT_MULTIPLIER;
+            this.analysisDetails.rewriteThreshold *= this.CODE_LIMIT_MULTIPLIER;
         }
 
         if (this.analysisDetails.mapOfFilesToConvert.size > 0) {
@@ -296,31 +312,29 @@ export class AnalysisTool {
     }
 
     /**
-     * Takes in a preparation report to tack onto the end of the recommendation.
-     * Based on analysis results and maxCodeLimit calculation, provides a recommendation.
-     * If app is a hybrid app, the recommendation is slightly modified. 
-     * @param preparationReport 
+     * Recommendation algorithm that checks type of application (AngularJS, Angular, or 
+     * hybrid), checks if sloc is under the rewriteThreshold, and checks if passes the 
+     * ngUpgrade requirements. Returns a recommendation with a preparation report only 
+     * when needed. 
+     * @param preparationReport preparation instructions for upgrading
      */
     public runRecommendation(preparationReport: string): string {
         let recommendation = "\x1b[4m\nYour Recommendation\n\x1b[0m";
         if (this.typeOfApplication() == "angular") {
             return "\x1b[34mThis is already an Angular application. You do not need to migrate.\x1b[0m";
         }
-
-        if (this.analysisDetails.maxCodeLimit >= this.analysisDetails.linesOfCode) {
-            
+        if (this.analysisDetails.rewriteThreshold >= this.analysisDetails.linesOfCode) {
             if (this.typeOfApplication() == "hybrid") {
                 recommendation += "\x1b[34mEven though you have already begun making a hybrid application with"
                     + " both AngularJS and Angular, the simplest solution is to rewrite your application from scratch.\n\x1b[0m";
             } else {
                 recommendation += "\x1b[34mThe simplest solution is to rewrite your application from scratch.\n\x1b[0m";
             }
-            //If preparationReport contains actual corrects, include it. 
             if (this.analysisDetails.mapOfFilesToConvert.size > 0 || !this.analysisDetails.hasUnitTest) {
                 recommendation += preparationReport + "\n";
             }
         } else {
-            if (this.passesngUpgradeRequirements()) {
+            if (this.passesNgUpgradeRequirements()) {
                 recommendation += "\x1b[34mYou have passed the necessary requirements and can use ngUpgrade as your migration path.\n\x1b[0m";
                 if (this.analysisDetails.angularElement == true) {
                     recommendation += "Continue using Angular Elements for components.";
@@ -351,7 +365,7 @@ export class AnalysisTool {
         return "angularjs";
     }
 
-    passesngUpgradeRequirements(): boolean {
+    passesNgUpgradeRequirements(): boolean {
         if (this.analysisDetails.rootScope == false &&
             this.analysisDetails.compile == false &&
             this.analysisDetails.hasUnitTest == true &&
